57e86f37308272ca5af64f4df223802f7cfec855
RJ <ryanjsuzuki@gmail.com>
2017-10-14 01:45:52 +0000

    Revert "remove shufflejs"
    This reverts commit 06ffb083f5e02c8adb72e51eae6f2015f83bfc23.

--- src/components/ShuffleView.js
+++ src/components/ShuffleView.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './ShuffleView.css';
 import Project from './Project.js';
-import projectimg1 from '../assets/img/projects/nck-website-small.png';
 import projectimg2 from '../assets/img/projects/cygna-site.png';
 
 class ShuffleView extends React.Component {
   render() {
     return (
         <div>
+
             <div className="row shuffle-grid" id="grid">
-                <Project title="Northern California Karate" imgSrc={projectimg1} alt="nck" data-groups='["web"]'/>
+                <Project title="Northern California Karate" imgSrc="http://placehold.it/300x100" alt="nck" data-groups='["web"]'/>
                 <Project title="Cygna Energy Services" imgSrc={projectimg2} alt="cygna" data-groups='["web"]'/>
                 <Project title="Flappy Bird Clone" imgSrc="http://placehold.it/300x100" alt="flappy" data-groups='["misc"]'/>
             </div>
